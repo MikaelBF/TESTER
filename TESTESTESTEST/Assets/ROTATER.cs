b597722621fd6f327a9bc2b3d6b2339453dad8a9
MikaelBF <mikaelbf@gmail.com>
2016-02-09 14:20:25 +0100

    Revert "Revert "Revert "Minor change"""
    This reverts commit 9b08fb0466b04efb7026d0c888b98b13dce6195a.

--- TESTESTESTEST/Assets/ROTATER.cs
+++ TESTESTESTEST/Assets/ROTATER.cs
@@ -7,6 +7,6 @@ public class ROTATER : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        gameObject.transform.Rotate(Vector3.up, 2);
+        gameObject.transform.Rotate(Vector3.up, 1);
 	}
 }

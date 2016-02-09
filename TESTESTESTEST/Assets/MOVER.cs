360c70b5f4622a9ea9db11dcb5aff9a2df70cdcc
MikaelBF <mikaelbf@gmail.com>
2016-02-09 13:11:46 +0100

    ScriptChanges

---
+++ TESTESTESTEST/Assets/MOVER.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MOVER : MonoBehaviour {
+    private float move = 1;
+	// Update is called once per frame
+	void Update () 
+    {
+        gameObject.transform.Translate(Vector3.left * move/10);
+        move += 1 * Time.deltaTime;
+        if (move > 3 || move < -3)
+        {
+            move = -move;
+        }
+	}
+}
